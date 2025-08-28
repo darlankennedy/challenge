c4481a44107115c56f11e3ef36b7c6fe5bb196d2
darlan <darlankennedy2@gmail.com>
2025-08-28 14:03:38 -0300

    chore: add docker

---
+++ start.sh
@@ -0,0 +1,15 @@
+composer install
+
+php artisan migrate --seed
+
+php artisan key:generate
+
+php artisan cache:clear
+php artisan config:clear
+php artisan route:clear
+php artisan view:clear
+php artisan event:clear
+php artisan optimize:clear
+
+
+php-fpm
